7f527acc532ee22061fe3bc79b0c76926440e2b3
ThakurVinayPundir <vinayrajput735@gmail.com>
2020-05-23 22:17:33 -0700

    problem statement added

--- Destiny.java
+++ Destiny.java
@@ -4,6 +4,36 @@ Codeforces Round #429 (Div. 1)
 problem D. Destiny using java implementation
 It is implemented using persistent segment tree.
 
+
+problem statement................
+
+Once, Leha found in the left pocket an array consisting of n integers, and in the right pocket q queries of the form l r k. If there are queries, then they must be answered. Answer for the query is minimal x such that x occurs in the interval l r strictly more than times or ?-?1 if there is no such number. Help Leha with such a difficult task.
+Input
+
+First line of input data contains two integers n and q (1?=?n,?q?=?3·105) — number of elements in the array and number of queries respectively.
+
+Next line contains n integers a 1,?a 2,?...,?a n (1?=?a i?=?n) — Leha's array.
+
+Each of next q lines contains three integers l, r and k (1?=?l?=?r?=?n,?2?=?k?=?5) — description of the queries.
+Output
+
+Output answer for each query in new line.
+Examples
+Input
+Copy
+
+4 2
+1 1 2 2
+1 3 2
+1 4 2
+
+Output
+Copy
+
+1
+-1
+
+
 */ 
 
 
